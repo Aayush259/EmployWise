d54143251f0a8213c9a0fe9a418401815ed34924
Aayush259 <aayushk4980@gmail.com>
2025-03-28 00:02:28 +0530

    Implement routing and authentication state management in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import Login from "./components/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { getToken } from "./utils/funcs";
 
 export default function App() {
 
+    const [loggedIn, setLoggedIn] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        setLoggedIn((getToken() ? true : false));
+    }, []);
+
+    if (loggedIn === null) return null;
+
     return (
-        <div></div>
+        <Routes>
+            <Route
+                path="/login"
+                element={loggedIn ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+                path="/"
+                element={loggedIn ? (
+                    <div>Welcome Back</div>
+                ) : (
+                    <Navigate to="/login" />
+                )}
+            />
+        </Routes>
     );
 };
