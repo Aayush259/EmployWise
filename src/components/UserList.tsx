8961649a7bee642002568490024224c351962e9f
Aayush259 <aayushk4980@gmail.com>
2025-03-28 23:54:01 +0530

    Update UserList component to navigate to edit page and set current user ID on button click

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,12 @@
 import { useUser } from "../context/UserContext";
 import { GoPencil } from "react-icons/go";
 import { Button, Loader } from "./Reusables";
+import { useNavigate } from "react-router-dom";
 
 export default function UserList() {
 
-    const { userList, fetchingUserList, currentPage, totalPages, error, setCurrentUser, nextPage, prevPage } = useUser();
+    const navigate = useNavigate();
+    const { userList, fetchingUserList, currentPage, totalPages, error, setCurrentUserId, nextPage, prevPage } = useUser();
 
     return (
         <div className="container mx-auto p-4">
@@ -23,13 +25,21 @@ export default function UserList() {
                             {
                                 userList.map((user) => (
                                     <div key={user.id} className="bg-gray-800 rounded-lg p-4 flex flex-col items-center group relative shadow-in duration-300">
-                                        <button className="opacity-0 group-hover:opacity-100 duration-200 absolute top-4 right-4 cursor-pointer hover:opacity-60" onClick={() => setCurrentUser(user.id)}>
+                                        <button
+                                            className="opacity-0 group-hover:opacity-100 duration-200 absolute top-4 right-4 cursor-pointer hover:opacity-60"
+                                            onClick={() => {
+                                                navigate("/edit");
+                                                setCurrentUserId(user.id)
+                                            }}
+                                        >
                                             <GoPencil size={20} />
                                         </button>
                                         <img
                                             src={user.avatar}
                                             alt={`${user.first_name} ${user.last_name}`}
-                                            className="w-auto h-auto rounded-full"
+                                            height={128}
+                                            width={128}
+                                            className="h-[128px] w-[128px] object-cover object-top rounded-full"
                                         />
                                         <h3 className="text-xl md:text-3xl font-semibold mt-6 mb-2">
                                             {user.first_name} {user.last_name}
